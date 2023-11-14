6e7c896b4f2282772c978616fb3a2352e742747b
Adewale shittu <sheriffhardewale@gmail.com>
2023-11-15 00:07:55 +0100

    complted login

--- components/landing-page/Login.jsx
+++ components/landing-page/Login.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import { HiOutlineEnvelope } from "react-icons/hi2";
 import { FaEye } from "react-icons/fa";
 import Link from 'next/link';
-const Login = () => {
+const LoginForm = () => {
   return (
     <div className='flex flex-col mx-auto w-[500px] space-y-4 justify-center items-center py-8'>
-        <h3 className='font-bold text-2xl text-black'>Create an Account</h3>
+        <h3 className='font-bold text-2xl text-black'>Welcome back</h3>
 
         <div className='w-full flex flex-col gap-3'>
             <label className='text-[#656C7A]' htmlFor="">
                 Email
             </label>
             <div className='flex relative border px-3 w-full py-3 rounded-md border-grey/200'>
-                <input type="text" className='outline-none w-full' placeholder='someone@gmail.com' />
+                <input type="text" className='outline-none w-full focus-within:bg-inherit' placeholder='someone@gmail.com' />
                 <HiOutlineEnvelope />
 
             </div>
@@ -23,7 +23,7 @@ const Login = () => {
                 Password
             </label>
             <div className='flex relative border px-3 w-full py-3 rounded-md border-grey/200'>
-                <input type="text" className='outline-none w-full' placeholder='someone@gmail.com' />
+                <input type="password" className='outline-none w-full' placeholder='someone@gmail.com' />
                 <FaEye />
 
             </div>
@@ -42,4 +42,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default LoginForm
